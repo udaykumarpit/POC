11e6b41da82aa5bfefd88d48573f2fcbcf6f48d4
Udaykumar Jewoor <udaykumargjewoor@gmail.com>
2024-10-03 15:39:37 +0530

    initial commit

--- service-a/src/main/java/com/routes/service_a/ServiceAApplication.java
+++ service-a/src/main/java/com/routes/service_a/ServiceAApplication.java
@@ -3,6 +3,8 @@ package com.routes.service_a;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,15 @@ public class ServiceAApplication {
 	}
 
 	@GetMapping("/service-a/greet")
-	@RateLimiter(name = "serviceA")
+	@RateLimiter(name = "serviceA", fallbackMethod = "rateLimitFallback")
 	public String greet() {
 		return "Hello from Service A!";
+	}
+
+	// Fallback method for Rate Limiter
+	public ResponseEntity<String> rateLimitFallback(Exception e) {
+		// Return a custom 429 response when the rate limit is exceeded
+		return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+				.body("Too many requests - please try again later.");
 	}
 }
--- service-b/src/main/java/com/routes/service_b/ServiceBApplication.java
+++ service-b/src/main/java/com/routes/service_b/ServiceBApplication.java
@@ -20,7 +20,7 @@ public class ServiceBApplication {
 	private final Bucket bucket;
 
 	//capacity = means the bucket can hold a maximum of 5 tokens at any given time. This is the capacity of the bucket.
-	//tokens = means that the bucket will attempt to add 20 tokens every minute.
+	//tokens = means that the bucket will attempt to add 5 tokens every minute.
 	public ServiceBApplication() {
 		Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1)));
 		this.bucket = Bucket4j.builder().addLimit(limit).build();
